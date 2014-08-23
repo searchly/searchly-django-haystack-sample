3857227d42b3f686a70ad06c82626de8675be931
Ferhat Sobay <ferhat.sobay@yahoo.com.tr>
2014-08-23 13:48:56 +0300

    format

--- django_haystack_sample/models.py
+++ django_haystack_sample/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Document(models.Model):
     name = models.CharField(max_length=40)
     body = models.TextField()
--- django_haystack_sample/search_indexes.py
+++ django_haystack_sample/search_indexes.py
@@ -1,6 +1,7 @@
 from haystack import indexes
 from django_haystack_sample.models import Document
 
+
 class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
